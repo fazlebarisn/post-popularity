e04c764c1bc63bb46ba72e60e817fbd3b74f0cb2
Fazle Bari <fazlebarisn@gmail.com>
2021-06-03 20:02:49 +0600

    complate a basic stacture

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+jQuery(function($){
+    $('#like_dislike a').on('click', function(){
+
+        let state = $(this).data('val');
+
+        $.ajax({
+            url: ajax_object.url,
+            method: 'post',
+            data:{
+                action: 'likes_dislikes_action',
+                post: ajax_object.post,
+                state: state,
+            }
+        }).success(function(e){
+            data = JSON.parse(e);
+            //console.log(data);
+            $('#like_dislike li').each(function(){
+                let state = $(this).find('a').data('val');
+                //console.log(state);
+                $(this).find('span').text("["+data[state]+"]");
+                //console.log($(this).find('span').text("["+data[state]+"]"));
+            });
+        });
+    });
+});
