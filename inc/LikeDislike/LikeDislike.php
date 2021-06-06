29c8973ac0c708f7d9384697ab8e2dbee93826b6
Fazle Bari <fazlebarisn@gmail.com>
2021-06-06 22:09:04 +0600

    remove var_dump

--- inc/LikeDislike/LikeDislike.php
+++ inc/LikeDislike/LikeDislike.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace Inc\LikeDislike;
-
-
 class LikeDislike
 {
 
@@ -16,13 +14,12 @@ class LikeDislike
 
             global $wpdb;
             global $post;
-            //var_dump($post->ID);
             $post_id = $post->ID;
             $table = $wpdb->prefix.TABLE_NAME;
 
             $likes = $wpdb->get_row( "SELECT COUNT(*) as likes FROM `{$table}` WHERE `post_id`={$post_id} AND `likes` !=0", ARRAY_A );
             $dislikes = $wpdb->get_row( "SELECT COUNT(*) as dislikes FROM `{$table}` WHERE `post_id`={$post_id} AND `dislikes` !=0", ARRAY_A );
-            //var_dump($likes , $dislikes );
+            
             $discription = "
                 <ul id='like_dislike'>
                     <li><a data-val='likes' href='javascript:;'>Like</a><span>[".$likes['likes']."]</span></li>
