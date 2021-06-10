ef2546fd40fdb21584c3e33f3607a914563ee9ee
Fazle Bari <fazlebarisn@gmail.com>
2021-06-10 12:02:49 +0600

    add css and js for dashboard page

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -7,7 +7,8 @@ use \Inc\Base\BaseController;
 class Enqueue extends BaseController
 {
 	public function register(){
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+        add_action( 'admin_enqueue_scripts', array( $this, 'adminEnqueue' ) );
 	}
 
 	function enqueue(){
@@ -18,5 +19,10 @@ class Enqueue extends BaseController
             'url' => admin_url('admin-ajax.php'),
             'post' => $post->ID
         ));
-	}
+    }
+    
+    public function adminEnqueue(){
+        wp_enqueue_style( 'likes_dislikes_style', $this->plugin_url . 'assets/css/app.css');
+        wp_enqueue_script( 'likes_dislikes_script', $this->plugin_url . 'assets/js/app.js', array('jquery'), '1.0.0', true);
+    }
 }
--- templates/admin.php
+++ templates/admin.php
@@ -3,12 +3,30 @@
 
 	<?php settings_errors() ?>
 
-    <form action="options.php" method="post" accept-charset="utf-8">
-        <?php
-            settings_fields( 'like_dislike_options_group' ); // option group id
-            do_settings_sections( 'like_dislike_menu' ); // page slug of section
-            submit_button();
-        ?>
-    </form>	
+	 <ul class="nav nav-tabs">
+	 	<li class="active"><a href="#tab-1">Manage Settings</a></li>
+	 	<li><a href="#tab-2">Updates</a></li>
+	 	<li><a href="#tab-3">About</a></li>
+	 </ul>
 
+	 <div class="tab-content">
+	 	<div id="tab-1" class="tab-pane active">
+			<form action="options.php" method="post" accept-charset="utf-8">
+				<?php
+					settings_fields( 'like_dislike_options_group' ); // option group id
+					do_settings_sections( 'like_dislike_menu' ); // page slug of section
+					submit_button();
+				?>
+			</form>	
+	 	</div>
+
+	 	<div id="tab-2" class="tab-pane">
+	 		<h3>Updates</h3>
+	 	</div>
+
+	 	<div id="tab-3" class="tab-pane">
+	 		<h3>About us</h3>
+	 	</div>
+
+	 </div>
 </div>
